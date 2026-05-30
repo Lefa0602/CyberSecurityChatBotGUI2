1e8272498778c85e37d85c7536b473f3baae71e5
Lefa0602 <molwantwalefa23@gmail.com>
2026-05-30 12:43:57 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace CyberSecurityChatbotGUI_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtChat = new System.Windows.Forms.RichTextBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtChat
+            // 
+            this.txtChat.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtChat.Location = new System.Drawing.Point(57, 118);
+            this.txtChat.Name = "txtChat";
+            this.txtChat.Size = new System.Drawing.Size(442, 214);
+            this.txtChat.TabIndex = 0;
+            this.txtChat.Text = "";
+            // 
+            // txtInput
+            // 
+            this.txtInput.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtInput.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(57, 371);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(442, 23);
+            this.txtInput.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnSend.Location = new System.Drawing.Point(553, 330);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(168, 61);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "SEND";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(107, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(442, 100);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.txtChat);
+            this.Name = "Form1";
+            this.Text = "Cybersecurity Awareness Bot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtChat;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace CyberSecurityChatbotGUI_2
+{
+    
+    public partial class Form1 : Form
+    {
+        string userName = "";
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            // Stores and formats user input
+            string userInput = txtInput.Text.ToLower().Trim();
+            
+
+
+            // Show user message
+            txtChat.AppendText("You: " + userInput + "\n");
+
+            string response = "";
+            // Memory feature: store user name
+            if (userInput.Contains("my name is"))
+            {
+                userName = userInput.Replace("my name is", "").Trim();
+                response = "Nice to meet you " + userName;
+            }
+            else if (userInput.Contains("hello"))
+            {
+                // CyberSecurity awareness responses
+                if (userName != "")
+                {
+                    response = "Welcome back " + userName;
+                }
+                else
+                {
+                    response = "Hello! What is your name?";
+                }
+            }
+            // Sentiment detection responses
+            else if (userInput.Contains("worried"))
+            {
+                response = "It's okay to feel worried. Staying informed helps you stay safe online.";
+            }
+            else if (userInput.Contains("scared"))
+            {
+                response = "Cyber threats can be scary, but being cautious and informed is the best protection.";
+            }
+            else if (userInput.Contains("curious"))
+            {
+                response = "Curiosity is great! Learning about cybersecurity helps protect you online.";
+            }
+            else if (userInput.Contains("phishing"))
+            {
+                int choice = random.Next(3);
+                if (choice == 0)
+                {
+                    response = "Phishing is a scam where attackers trick you into giving personal information.";
+                }
+                else if (choice == 1)
+                {
+                    response = "Be careful of suspicious emails and fake websites pretending to be trusted companies.";
+                }
+                else
+                {
+                    response = "Never click unkown links or share passwords through suspicious messages.";
+                }
+            }
+            else if (userInput.Contains("password"))
+            {
+                response = "Use strong passwords with letters, numbers, and symbols.";
+            }
+            else if (userInput.Contains("scam"))
+            {
+                response = "Online scams try to trick people into sending money or personal information.";
+            }
+            else if (userInput.Contains("link"))
+            {
+                response = "Avoid clicking suspicious links.";
+            }
+            else
+            {
+                response = "I don't understand. Try asking about cybersecurity.";
+            }
+                
+
+            txtChat.AppendText("Bot: " + response + "\n\n");
+
+            // Show bot response
+            txtChat.AppendText("Bot: " + response + "\n\n");
+
+            // Clear input
+            txtInput.Clear();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtChat.AppendText("====================================\n");
+            txtChat.AppendText(" CYBERSECURITY AWARENESS BOT\n");
+            txtChat.AppendText("====================================\n\n");
+
+            txtChat.AppendText("Bot: Welcome! Ask me about cybersecurity.\n\n");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
